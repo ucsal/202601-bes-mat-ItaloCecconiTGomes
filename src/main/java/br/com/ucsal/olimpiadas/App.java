de38a2b12502a9ac027c4fd9b25916093795e2eb
italo cecconi teixeira gomes <italocecconiteixeiragomes@gmail.com>
2026-04-03 17:42:57 -0300

    Retirada de comentario desnecessario

--- src/main/java/br/com/ucsal/olimpiadas/App.java
+++ src/main/java/br/com/ucsal/olimpiadas/App.java
@@ -30,30 +30,7 @@ public class App {
 		Menu menu = new Menu();
 		menu.exibirMenu();
 		
-		/*
-		while (true) {
-			System.out.println("\n=== OLIMPÍADA DE QUESTÕES (V1) ===");
-			System.out.println("1) Cadastrar participante");
-			System.out.println("2) Cadastrar prova");
-			System.out.println("3) Cadastrar questão (A–E) em uma prova");
-			System.out.println("4) Aplicar prova (selecionar participante + prova)");
-			System.out.println("5) Listar tentativas (resumo)");
-			System.out.println("0) Sair");
-			System.out.print("> ");
-
-			switch (in.nextLine()) {
-			case "1" -> new CadastrarParticipanteAcao().executar(in);
-			case "2" -> new CadastrarProvaAcao().executar(in);
-			case "3" -> new CadastrarQuestaoAcao().executar(in);
-			case "4" -> new AplicarProvaAcao().executar(in);
-			case "5" -> new ListarTentativasAcao().executar(in);
-			case "0" -> {
-				System.out.println("tchau");
-				return;
-			}
-			default -> System.out.println("opção inválida");
-			}
-		}*/
+		
 	}
 
 }
